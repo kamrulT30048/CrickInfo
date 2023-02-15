ff7ac0aed5ef853f679a7ada9188c66aff9cc95e
kamrulT30048 <kamrul.hasan@bjitacademy.com>
2023-02-15 17:09:20 +0600

    updated

---
+++ app/src/main/java/com/kamrulhasan/crickinfo/ui/fragment/MatchDetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.kamrulhasan.crickinfo.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kamrulhasan.crickinfo.R
+import com.kamrulhasan.crickinfo.databinding.FragmentFixturesBinding
+import com.kamrulhasan.crickinfo.databinding.FragmentMatchDetailsBinding
+
+class MatchDetailsFragment : Fragment() {
+
+    private var _binding: FragmentMatchDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentMatchDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
